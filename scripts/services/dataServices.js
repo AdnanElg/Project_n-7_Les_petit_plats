61d9aab247bfd041692d8d02ff38b975eaecc937
El Ghalbzouri Adnan <alghalbzouri@santeformapro.com>
2023-06-16 15:04:40 +0200

    mises en place de eslint

--- scripts/services/dataServices.js
+++ scripts/services/dataServices.js
@@ -2,91 +2,91 @@
 import { dataRecipes } from "../data/dataRecipes.js";
 
 export const dataServices = {
-  //? Fonction pour récupérer toutes les recettes
-  async getAllRecipes() {
-    try {
-      //? Affichage des données pour vérification
-      // console.log(dataRecipes);
-      //? Retour des données
-      return dataRecipes;
-    } catch (err) {
-      //? En cas d'erreur, affichage d'un message explicite
-      throw new Error(`Error getting recipes: ${err.message}`);
-    }
-  },
+	//? Fonction pour récupérer toutes les recettes
+	async getAllRecipes() {
+		try {
+			//? Affichage des données pour vérification
+			// console.log(dataRecipes);
+			//? Retour des données
+			return dataRecipes;
+		} catch (err) {
+			//? En cas d'erreur, affichage d'un message explicite
+			throw new Error(`Error getting recipes: ${err.message}`);
+		}
+	},
 
-  async getAllIngredients() {
-    try {
-      //? Récupération de tous les ingrédients dans un tableau d'arrays et les transforme en minuscule
-      const ingredientsArrays = dataRecipes
-        .map((recipe) => recipe.ingredients)
-        .flat()
-        .map((ingredient) => ingredient.ingredient.toLowerCase());
+	async getAllIngredients() {
+		try {
+			//? Récupération de tous les ingrédients dans un tableau d'arrays et les transforme en minuscule
+			const ingredientsArrays = dataRecipes
+				.map((recipe) => recipe.ingredients)
+				.flat()
+				.map((ingredient) => ingredient.ingredient.toLowerCase());
 
-      //? Création d'un Set d'ingrédients pour éviter les doublons
-      const allIngredientsSet = new Set(ingredientsArrays);
+			//? Création d'un Set d'ingrédients pour éviter les doublons
+			const allIngredientsSet = new Set(ingredientsArrays);
 
-      //? Conversion du Set en tableau
-      const allIngredients = Array.from(allIngredientsSet);
+			//? Conversion du Set en tableau
+			const allIngredients = Array.from(allIngredientsSet);
 
-      //? Affichage des données pour vérification
-      // console.log(allIngredients);
-      //? Retour des données
-      return allIngredients;
-    } catch (err) {
-      //? En cas d'erreur, affichage d'un message explicite
-      throw new Error(`Error getting ingredients: ${err.message}`);
-    }
-  },
+			//? Affichage des données pour vérification
+			// console.log(allIngredients);
+			//? Retour des données
+			return allIngredients;
+		} catch (err) {
+			//? En cas d'erreur, affichage d'un message explicite
+			throw new Error(`Error getting ingredients: ${err.message}`);
+		}
+	},
 
-  //? Fonction pour récupérer tous les appareils
-  async getAllAppliance() {
-    try {
-      //? Récupération de tous les appareils dans un tableau et les transforme en minuscule
-      const applianceArrays = dataRecipes.map((recipe) =>
-        recipe.appliance.toLowerCase()
-      );
+	//? Fonction pour récupérer tous les appareils
+	async getAllAppliance() {
+		try {
+			//? Récupération de tous les appareils dans un tableau et les transforme en minuscule
+			const applianceArrays = dataRecipes.map((recipe) =>
+				recipe.appliance.toLowerCase()
+			);
 
-      //? Création d'un Set d'appliance pour éviter les doublons
-      const allApplianceSet = new Set(applianceArrays);
+			//? Création d'un Set d'appliance pour éviter les doublons
+			const allApplianceSet = new Set(applianceArrays);
 
-      //? Conversion du Set en tableau
-      const allAppliance = Array.from(allApplianceSet);
+			//? Conversion du Set en tableau
+			const allAppliance = Array.from(allApplianceSet);
 
-      //? Affichage des données pour vérification
-      // console.log(allAppliance);
+			//? Affichage des données pour vérification
+			// console.log(allAppliance);
 
-      //? Retour des données
-      return allAppliance;
-    } catch (err) {
-      //? En cas d'erreur, affichage d'un message explicite
-      throw new Error(`Error getting appliance: ${err.message}`);
-    }
-  },
+			//? Retour des données
+			return allAppliance;
+		} catch (err) {
+			//? En cas d'erreur, affichage d'un message explicite
+			throw new Error(`Error getting appliance: ${err.message}`);
+		}
+	},
 
-  //? Fonction pour récupérer tous les ustensiles
-  async getAllUstensils() {
-    try {
-      //? Récupération de tous les ustensiles dans un tableau et les transforme en minuscule
-      const ustensilsArrays = dataRecipes
-        .map((recipe) => recipe.ustensils)
-        .flat()
-        .map((ustensil) => ustensil.toLowerCase());
+	//? Fonction pour récupérer tous les ustensiles
+	async getAllUstensils() {
+		try {
+			//? Récupération de tous les ustensiles dans un tableau et les transforme en minuscule
+			const ustensilsArrays = dataRecipes
+				.map((recipe) => recipe.ustensils)
+				.flat()
+				.map((ustensil) => ustensil.toLowerCase());
 
-      //? Création d'un Set d'ustensils pour éviter les doublons
-      const allUstensilsSet = new Set(ustensilsArrays);
+			//? Création d'un Set d'ustensils pour éviter les doublons
+			const allUstensilsSet = new Set(ustensilsArrays);
 
-      //? Conversion du Set en tableau
-      const allUstensils = Array.from(allUstensilsSet);
+			//? Conversion du Set en tableau
+			const allUstensils = Array.from(allUstensilsSet);
 
-      //? Affichage des données pour vérification
-      // console.log(allUstensils);
+			//? Affichage des données pour vérification
+			// console.log(allUstensils);
 
-      //? Retour des données
-      return allUstensils;
-    } catch (err) {
-      //? En cas d'erreur, affichage d'un message explicite
-      throw new Error(`Error getting ustensils: ${err.message}`);
-    }
-  },
+			//? Retour des données
+			return allUstensils;
+		} catch (err) {
+			//? En cas d'erreur, affichage d'un message explicite
+			throw new Error(`Error getting ustensils: ${err.message}`);
+		}
+	},
 };
